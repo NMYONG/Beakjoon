73eee9094f9ddb8637f165fa9c5acbea348c1ec7
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-02-22 10:50:52 +0900

    [Gold IV] Title: N-Queen, Time: 30864 ms, Memory: 209292 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,24 +1,30 @@
 import sys
 input = sys.stdin.readline
 
-def possible(row, col):
-    for r in range(row):
-        if board[r] == col or abs(row - r) == abs(col - board[r]):
+
+def possible(r, c):
+    for i in range(r):
+        if c == visit[i] or abs(i - r) == abs(visit[i] - c):
             return False
     return True
+
 
 def backtracking(row):
     global cnt
     if row == N:
         cnt += 1
-        return
-    for col in range(N):
-        if possible(row, col):
-            board[row] = col
-            backtracking(row + 1)
+
+    else:
+        for j in range(N):
+            visit[row] = j
+            if possible(row, j):
+                backtracking(row + 1)
+
 
 N = int(input())
 cnt = 0
-board = [-1] * N
+
+visit = [0]*N
 backtracking(0)
+
 print(cnt)
