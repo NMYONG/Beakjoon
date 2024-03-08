d0b48bdc84369bb7dc3205e6a4d5e258267d0840
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-03-08 23:05:17 +0900

    [Silver II] Title: DFS와 BFS, Time: 448 ms, Memory: 32236 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,44 @@
-from collections import deque
+def dfs(c):
+    ans_dfs.append(c)   # 방문한 노드 추가
+    visited[c] = 1      # 방문 표시
+
+    for n in adj[c]:    # 방문한 노드에 대해서 연결된 노드들을 검사
+        if visited[n] == 0: # 방문하지 않았으면
+            dfs(n)
+
+def bfs(s):
+    q = []              # 큐 생성
+    q.append(s)
+
+    ans_bfs.append(s)
+    visited[s] = 1      # 방문 표시
+
+    while q:
+        c = q.pop(0)
+        for n in adj[c]:
+            if visited[n] == 0: # 방문하지 않은 노드라면 q에 삽입
+                q.append(n)
+                ans_bfs.append(n)
+                visited[n] = 1
 
 N, M, V = map(int, input().split())
-
-graph = [[False] * (N + 1) for _ in range(N + 1)]
+adj = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    v1, v2 = map(int, input().split())
+    adj[v1].append(v2)
+    adj[v2].append(v1)  # 양방향
 
-visited1 = [False] * (N + 1)  # dfs의 방문기록
-visited2 = [False] * (N + 1)  # bfs의 방문기록
+for i in range(1, N+1): # 오름차순 정렬
+    adj[i].sort()
 
-
-def bfs(V):
-    q = deque([V])  # pop메서드의 시간복잡도가 낮은 덱 내장 메서드를 이용한다
-    visited2[V] = True  # 해당 V 값을 방문처리
-    while q:  # q가 빌때까지 돈다.
-        V = q.popleft()  # 큐에 있는 첫번째 값 꺼낸다.
-        print(V, end=" ")  # 해당 값 출력
-        for i in range(1, N + 1):  # 1부터 N까지 돈다
-            if not visited2[i] and graph[V][i]:  # 만약 해당 i값을 방문하지 않았고 V와 연결이 되어 있다면
-                q.append(i)  # 그 i 값을 추가
-                visited2[i] = True  # i 값을 방문처리
-
-
-def dfs(V):
-    visited1[V] = True  # 해당 V값 방문처리
-    print(V, end=" ")
-    for i in range(1, N + 1):
-        if not visited1[i] and graph[V][i]:  # 만약 i값을 방문하지 않았고 V와 연결이 되어 있다면
-            dfs(i)  # 해당 i 값으로 dfs를 돈다.(더 깊이 탐색)
-
-
+visited = [0] * (N + 1)
+ans_dfs = []
 dfs(V)
-print()
+
+visited = [0] * (N + 1)
+ans_bfs = []
 bfs(V)
+
+print(*ans_dfs)
+print(*ans_bfs)
