b1d7824ea5fa40da2b4f8c5c90b8576511f3df17
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-03 00:51:32 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,14 +1,17 @@
-def dfs(n, s, lst):
-    if n == M:
-        ans.append(lst)
+def dfs(n, lst):
+    if n > N:
+        if len(lst) == M:
+            ans.append(lst)
         return
 
-    for i in range(s, N+1):
-        dfs(n+1, i+1, lst+[i])
-
+    # 선택하는 경우
+    dfs(n+1, lst+[n])
+    # 선택하지 않는 경우
+    dfs(n+1, lst)
 
 N, M = map(int, input().split())
 ans = []
-dfs(0, 1, [])
+dfs(1, [])
+
 for i in ans:
     print(*i)
