bd7aec6c705eb23fe009d9d423cb34fe7e5d725f
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-08 15:30:07 +0900

    [Silver III] Title: N과 M (3), Time: 992 ms, Memory: 271468 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -4,11 +4,11 @@ def dfs(n, lst):
         return
 
     for i in range(1, N+1):
-        dfs(n+1, lst+[i])
-
+        dfs(n+1, lst + [i])
 
 N, M = map(int, input().split())
 ans = []
+
 dfs(0, [])
 
 for i in ans:
