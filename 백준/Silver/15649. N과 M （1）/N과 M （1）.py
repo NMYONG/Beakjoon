aa37c28b1869772f6ecc101ab649e0620a7dbe54
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-08 15:14:20 +0900

    [Silver III] Title: N과 M (1), Time: 204 ms, Memory: 36580 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -3,16 +3,17 @@ def dfs(n, lst):
         ans.append(lst)
         return
 
-    for j in range(1, N+1):
-        if visited[j] == 0:
-            visited[j] = 1
-            dfs(n+1, lst+[j])
-            visited[j] = 0
+    for i in range(1, N+1):
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(n+1, lst+[i])
+            visited[i] = 0
 
 N, M = map(int, input().split())
 ans = []
 visited = [0] * (N+1)
 
 dfs(0, [])
-for lst in ans:
-    print(*lst)
+
+for i in ans:
+    print(*i)
