c8b6a1c34294988425c316679441599d2c4af46a
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-22 00:36:24 +0900

    [Silver I] Title: 트리 순회, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,6 +1,10 @@
-# 1991 트리 순회
 N = int(input())
 TREE = {}
+
+ansPreorder = []
+ansInorder = []
+ansPostorder = []
+
 for _ in range(N):
     root, left, right = input().split()
     TREE[root] = [left, right]
@@ -8,27 +12,26 @@ for _ in range(N):
 # 전위 순회
 def preOrder(root):
     if root != '.':
-        print(root, end='')
+        ansPreorder.append(root)
         preOrder(TREE[root][0])
         preOrder(TREE[root][1])
 
 def inOrder(root):
     if root != '.':
         inOrder(TREE[root][0])
-        print(root, end='')
+        ansInorder.append(root)
         inOrder(TREE[root][1])
 
 def postOrder(root):
     if root != '.':
         postOrder(TREE[root][0])
         postOrder(TREE[root][1])
-        print(root, end='')
+        ansPostorder.append(root)
 
 preOrder('A')
-print()
 inOrder('A')
-print()
 postOrder('A')
 
-
-
+print(''.join(map(str, ansPreorder)))
+print(''.join(map(str, ansInorder)))
+print(''.join(map(str, ansPostorder)))
