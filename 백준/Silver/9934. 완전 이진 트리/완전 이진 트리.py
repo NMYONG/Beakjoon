597d248af12526575a89e15f9b0572a3b32a31b0
NMYONG <140034014+NMYONG@users.noreply.github.com>
2024-04-23 17:45:34 +0900

    [Silver I] Title: 완전 이진 트리, Time: 112 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,18 +1,17 @@
 def dfs(lst, level):
-    midIndex = len(lst) // 2 # 배열의 가운데 인덱스 구하기
+    midIndex = len(lst) // 2
     ans[level].append(lst[midIndex])
 
-    if len(lst) == 1: # 종료조건
+    if len(lst) == 1:
         return
 
-		# 가운데 인덱스를 기준으로 좌, 우로 나눠서 다시 dfs 돌린다.
     dfs(lst[:midIndex], level + 1)
     dfs(lst[midIndex + 1:], level + 1)
 
 K = int(input()) # level
 numList = list(map(int, input().split()))
 
-ans = [[] for _ in range(K)] # 정답을 담을 배열, level의 개수만큼 생성
+ans = [[] for _ in range(K)]
 
 dfs(numList, 0)
 
